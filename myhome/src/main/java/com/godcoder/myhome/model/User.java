b9a633383fe3beddf6de8fae727243e5401d8569
HyeYeong <hy0237@uab.edu>
2021-11-27 05:41:22 +0900

    spring onetoMany jpa

--- myhome/src/main/java/com/godcoder/myhome/model/User.java
+++ myhome/src/main/java/com/godcoder/myhome/model/User.java
@@ -1,6 +1,7 @@
 package com.godcoder.myhome.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -24,6 +25,10 @@ public class User {
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
 
+    @JsonIgnore
     private List<Role> roles = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Board> boards = new ArrayList<>();
 
 }
